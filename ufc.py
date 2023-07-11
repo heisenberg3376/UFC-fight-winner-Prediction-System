a9a8ceacbba7900643e6e086a85c31b237716c38
Phanendra Sai Ram <139060185+Phani8910@users.noreply.github.com>
2023-07-11 20:10:01 +0530

    Update ufc.py

--- ufc.py
+++ ufc.py
@@ -167,7 +167,7 @@ elif selected=="Did'nt Find your fighter?":
 
    l,m,r = st.columns((2,1,2))
    with l:
-      fighter_1 = st.selectbox(names['Fighter_fullName'])
+      fighter_1 = st.selectbox('select a fighter for red corner',names['Fighter_fullName'])
    with r:
-      fighter_2 = st.selectbox(names['Fighter_fullName'])
+      fighter_2 = st.selectbox('select a fighter for red corner',names['Fighter_fullName'])
    st.markdown('''# Find him''')
