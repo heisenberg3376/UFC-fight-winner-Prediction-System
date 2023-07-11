e7a0c11367f1b2ca7af22c2381fdafa4c8b942eb
Phanendra Sai Ram <139060185+Phani8910@users.noreply.github.com>
2023-07-11 19:44:32 +0530

    Update ufc.py

--- ufc.py
+++ ufc.py
@@ -15,7 +15,8 @@ selected_fighter_2 = 0
 fighter_data = pd.read_csv('raw_fighter_details.csv')
 
 st.set_page_config(layout='wide')
-with option_menu('Main Menu',['Home',"Did'nt Find your fighter"])
+with st.sidebar:
+   selected = option_menu('Main Menu',['Home',"Did'nt Find your fighter"])
    
 st.header('UFC fight Winner Prediction System')
 #fighter_df = pd.read_json('fighters.json',lines = True)[['name','wins','losses','draws']]
